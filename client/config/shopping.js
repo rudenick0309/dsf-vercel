9207b14a71d3cb969d2296ea5d75f604125d5da8
KoYunHyuk <sksgur3217@gmail.com>
2020-07-06 15:26:08 +0900

    marge

---
+++ client/config/shopping.js
@@ -0,0 +1,7 @@
+// 네이버 쇼핑 API
+// 절대로 API KEY를 GitHub에 push하는 일은 없어야 합니다.
+// API KEY는 여러분이 사용하는 서비스의 접근 권한을 부여하는 열쇠로, 비밀번호처럼 다뤄야 합니다.
+
+export const YOUTUBE_API_KEY =
+  "https://developers.naver.com/docs/search/shopping/";
+// YOUTUBE_API_KEY >> naver shopping API 로 수정하기
