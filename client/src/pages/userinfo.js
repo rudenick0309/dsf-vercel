7c3619e4a2f87499125f3a7ee30df726f1f775f5
goodlana <lahee13579@gmail.com>
2020-07-07 21:12:13 +0900

    클라이언트 경로 변경

--- client/src/pages/userinfo.js
+++ client/src/pages/userinfo.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
-import {Viewinfo} from '../Component/viewinfo'
+import {Viewinfo} from '../component/viewinfo'
 
 //const axios = require('axios'); 
 
