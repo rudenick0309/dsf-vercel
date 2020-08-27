df100e17847c21ea02d8fc04bafe758c4652cd4c
rudenick0309 <rudenick0309@gmail.com>
2020-08-28 01:06:19 +0900

    Add : home component

---
+++ client/pages/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Head from "next/head";
+import AppLayout from "../components/AppLayout";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>DessertFinder</title>
+      </Head>
+      <AppLayout>
+        <div>Home 테스트</div>
+      </AppLayout>
+
+    </>
+
+  );
+};
+
+export default Home;
