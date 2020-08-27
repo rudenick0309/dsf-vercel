8290c54fe9c2f1df441e7e901d0205797d7e75fa
rudenick0309 <rudenick0309@gmail.com>
2020-08-28 01:06:00 +0900

    Add : signup component

---
+++ client/pages/signup.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Head from "next/head";
+import AppLayout from "../components/AppLayout";
+
+const signup = () => {
+  return (
+    <>
+      <Head>
+        <title>signup | DessertFinder</title>
+      </Head>
+      <AppLayout>
+        <div>signup 테스트</div>
+      </AppLayout>
+
+    </>
+
+  );
+};
+
+export default signup;
