54e2699d32da1c382f0fb574fa3fa03c3b3e934f
rudenick0309 <rudenick0309@gmail.com>
2020-08-28 13:51:07 +0900

    Fix: this binding error, no re-rendering

--- client/store/store.js
+++ client/store/store.js
@@ -1,30 +1,32 @@
 const { observable } = require('mobx');
 
-const oneTestStore = observable({
-  isOneTest : false,
+const oneStore = observable({
+  isOne : false,
   data : null,
-  oneTestFunction(data) {
-    console.log('In mobx store, data : ', data)
-    this.isOneTest = true;
+
+  oneFunction(data) {
+    this.isOne = true;
     setTimeout(() => {
       this.data = data;
-      this.isOneTest = false;
-      // twoTestStore.data.push(777777)
+      this.isOne = false;
     }, 3000)
-  }
-})
-
-const twoTestStore = observable({
-  isTwoTest : false,
-  data : null,
-  twoTestFunction(data) {
-    this.isTwoTest = true;
-    // setTimeout(() => {
-      this.data = data;
-      this.isTwoTest = false;
-    // }, 3000)
-  }
+  },
 })
 
 
-export { oneTestStore, twoTestStore };
+export { oneStore };
+
+
+
+//
+// const twoTestStore = observable({
+//   isTwoTest : false,
+//   data : null,
+//   twoTestFunction(data) {
+//     this.isTwoTest = true;
+//     // setTimeout(() => {
+//       this.data = data;
+//       this.isTwoTest = false;
+//     // }, 3000)
+//   }
+// })
