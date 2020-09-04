abb63029a9119cf3226aa56dcffcf654d5d6a30c
rudenick0309 <rudenick0309@gmail.com>
2020-09-04 11:26:53 +0900

    Add: initial setting

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,18 +1,20 @@
-import * as React from "react";
+import React from "react";
 import Head from "next/head";
 import AppLayout from "../components/AppLayout";
-import img from '../image/images.jpg'
+import {img} from '../image/images.jpg'
+
+
+// const iImaage
 
 const Home = () => {
-  const i = img
   return (
     <>
       <Head>
         <title>DessertFinder</title>
       </Head>
       <AppLayout>
-        <div>Home 테스트</div>
-        <image src={i}/>
+        <div>Home test</div>
+        <div style={{width:'600px', height:'600px'}}><image src={img} style={{width:'100px', height:'100px'}} /></div>
       </AppLayout>
 
     </>
