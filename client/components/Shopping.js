46d82dde6499ebf67476dfe0b43efb775ae45fac
rudenick0309 <rudenick0309@gmail.com>
2020-09-12 01:43:14 +0900

    Style: modified the entity layout - changed main page component from 'login component' to 'map component'. In map component, I had deleted unused function, console and some code.

--- client/components/Shopping.js
+++ client/components/Shopping.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
+import {map } from '../store/map'
 
 const Shopping = () => {
+  console.log('f shopping, culturename', map.cultureName)
   return (
     <>
       <div>Shopping 테스트</div>
