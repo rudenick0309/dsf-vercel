7b60c5cf838788d75beaa42a7c00b56960d3c633
rudenick0309 <rudenick0309@gmail.com>
2020-09-12 17:46:31 +0900

    Feat: added the results of api to component. One has a 'the result of blog search', the other has 'the result of shopping search'. results are not the perfect. Because They have unappropriate values of the entity value. So, I will deleted this values.

--- client/components/AppLayout.js
+++ client/components/AppLayout.js
@@ -1,57 +1,106 @@
+import React from "react";
 
-import React from 'react';
-
-import {Menu,Input, Row, Col} from 'antd';
-import Link from 'next/link'
-import styled from 'styled-components';
+import {Menu, Input, Row, Col} from "antd";
+import Link from "next/link";
+import styled from "styled-components";
 import Map from "./Map";
+import Shopping from "./Shopping";
+import Blog from "./Blog";
+import {Layout} from "antd";
+
+const {Header, Footer, Sider, Content} = Layout;
+
 
 const DivStd = styled.div`
   display: flex;
   justify-content: flex-end;
-`
+`;
 
 const MenuStd = styled(Menu)`
-`
+`;
 
 const MenuStdChildren = styled(Menu)`
   justify-content: flex-end;
-`
+`;
+
+const ContentStyled = styled(Content)`
+  border: 3px solid green;
+  width:40%;
+`;
 
 const AppLayout = ({children}) => {
+
   return (
     <>
-      <DivStd>
+      <Layout>
+        <Header>
+          <DivStd>
 
-        <MenuStd mode='horizontal'>
+            <MenuStd mode='horizontal'>
 
-          <MenuStdChildren.Item>
-            <Link href='/'><a>Home</a></Link>
-          </MenuStdChildren.Item>
+              <MenuStdChildren.Item>
+                <Link href='/'><a>Home</a></Link>
+              </MenuStdChildren.Item>
 
-          <MenuStdChildren.Item>
-            <Link href='/signin'><a>sign in</a></Link>
-          </MenuStdChildren.Item>
+              <MenuStdChildren.Item>
+                <Link href='/signin'><a>sign in</a></Link>
+              </MenuStdChildren.Item>
 
-          <MenuStdChildren.Item>
-            <Link href='/signup'><a>sign up</a></Link>
-          </MenuStdChildren.Item>
+              <MenuStdChildren.Item>
+                <Link href='/signup'><a>sign up</a></Link>
+              </MenuStdChildren.Item>
 
-        </MenuStd>
+            </MenuStd>
 
-      </DivStd>
+          </DivStd>
+        </Header>
+        <Layout>
+          <Sider><Blog/></Sider>
+          <ContentStyled><Map/></ContentStyled>
+          <Sider><Blog/></Sider>
+        </Layout>
+        <Footer><Shopping/></Footer>
+      </Layout>
 
 
-      <Row gutter={8}>
+      {/*<DivStd>*/}
 
-        <Col xs={24} md={2} />
-        <Col xs={24} md={16}><Map/></Col>
-        <Col xs={24} md={6}>{children}</Col>
+      {/*  <MenuStd mode='horizontal'>*/}
 
-      </Row>
+      {/*    <MenuStdChildren.Item>*/}
+      {/*      <Link href='/'><a>Home</a></Link>*/}
+      {/*    </MenuStdChildren.Item>*/}
+
+      {/*    <MenuStdChildren.Item>*/}
+      {/*      <Link href='/signin'><a>sign in</a></Link>*/}
+      {/*    </MenuStdChildren.Item>*/}
+
+      {/*    <MenuStdChildren.Item>*/}
+      {/*      <Link href='/signup'><a>sign up</a></Link>*/}
+      {/*    </MenuStdChildren.Item>*/}
+
+      {/*  </MenuStd>*/}
+
+      {/*</DivStd>*/}
+
+
+      {/*<Row gutter={4}>*/}
+      {/*  <Col xs={24} md={2} />*/}
+      {/*  <Col xs={24} md={16}><Map/></Col>*/}
+      {/*  <Col xs={24} md={4}><Blog/></Col>*/}
+      {/*  <Col xs={24} md={2} />*/}
+      {/*</Row>*/}
+
+      {/*<Row gutter={8} >*/}
+
+      {/*  <Col xs={24} md={2} />*/}
+      {/*  <Col xs={24} md={16}><Shopping/></Col>*/}
+      {/*  <Col xs={24} md={6} />*/}
+
+      {/*</Row>*/}
 
     </>
-  )
-}
+  );
+};
 
 export default AppLayout;
